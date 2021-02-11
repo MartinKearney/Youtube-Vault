a1481c77617692929f2f69728c5a21d3ebee79b4
MartinKearney <martin-kearney@hotmail.co.uk>
2021-02-11 17:53:50 +0000

    Main structure of server

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const app = express();
+
+// connectDB();
+
+// app.use(express.json());
+
+// Define API endpoints or import handler files
+// app.use('/api/auth', require('./routes/auth'));
 
 app.get('/', (req, res) => res.json({ msg: 'Hello!' }));
 
